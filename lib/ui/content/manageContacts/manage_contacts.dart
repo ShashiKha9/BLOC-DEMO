c2acd3ce24d194c916e6d268b52950cb832b19de
Janmejay Parmar <janmejay24parmar@gmail.com>
2024-04-01 13:56:41 +0530

    + No records toast shown if no data is present on GetManageContactsSuccessState

--- lib/ui/content/manageContacts/manage_contacts.dart
+++ lib/ui/content/manageContacts/manage_contacts.dart
@@ -61,8 +61,10 @@ class ManageGroupContactsContent extends BaseModalRouteState {
                 _loadingController.hide();
                 if (state is GetManageContactsSuccessState) {
                   tableData.clear();
+                  if (state.manageContactsData.isEmpty) {
+                    ToastDialog.warning("No records found");
+                  }
                   tableData = state.manageContactsData;
-
                   if (branchData.isEmpty) {
                     branchData.add(ContactBranch(name: "All"));
                     Set<String> uniqueIds = {};
