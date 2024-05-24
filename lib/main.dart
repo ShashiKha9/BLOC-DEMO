92cd90f534308fd8f7d4be34c60ac11acef8ab8d
Janmejay Parmar <janmejay24parmar@gmail.com>
2024-05-24 10:41:51 +0530

    + maukaUrl removed from configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() async {
   DependencyConfiguration configuration = DependencyConfiguration();
   runApp(await configuration.setup(ProjectConfiguration(
       baseUrl: "https://connect2dev.wisper.com/",
-        maukaUrl: "https://chatdev.mauka.services",
       environment: Environment.dev,
       stripePublishableKey: "pk_test_8YBc9wx4Nalwqn65XXbWfARY")));
 }
